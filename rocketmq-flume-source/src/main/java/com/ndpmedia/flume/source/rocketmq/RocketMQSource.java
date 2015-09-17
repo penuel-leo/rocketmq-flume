56de6ccae629f9849604b44ba5b7966a22a60d3c
penuel <penuel.leo@gmail.com>
2015-09-17 12:13:34 +0800

    source test

---
+++ rocketmq-flume-source/src/main/java/com/ndpmedia/flume/source/rocketmq/RocketMQSource.java
@@ -0,0 +1,12 @@
+package com.ndpmedia.flume.source.rocketmq;
+
+/**
+ * RocketMQSource Created with mythopoet.
+ *
+ * @author penuel (penuel.leo@gmail.com)
+ * @date 15/9/17 下午12:13
+ * @desc
+ */
+public class RocketMQSource {
+
+}
