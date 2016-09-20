a5a46ae723b6991f1c9b80c785d1d4f1d66254f4
Li Zhanhui <lizhanhui@gmail.com>
2016-09-20 16:09:58 +0800

    Bug fix

--- rocketmq-flume-source/src/main/java/com/ndpmedia/flume/source/rocketmq/ProcessQueue.java
+++ rocketmq-flume-source/src/main/java/com/ndpmedia/flume/source/rocketmq/ProcessQueue.java
@@ -135,7 +135,11 @@ public class ProcessQueue {
 
         lock.readLock().lock();
         try {
-            return window.last() - window.first();
+            if (window.isEmpty()) {
+                return 0;
+            } else {
+                return window.last() - window.first();
+            }
         } finally {
             lock.readLock().unlock();
         }
